1664c78ce0e6ed411fd50b244fd602b9d64dfb96
Taylor L Beckham <97658753+tlbeckham@users.noreply.github.com>
2025-02-26 07:01:58 +0000

    working on backgroun vs foreground

--- root/home/master/smallsh.c
+++ root/home/master/smallsh.c
@@ -172,6 +172,8 @@ void other_commands(struct command_line *curr_command)
 		// The child process executes this branch
 		// printf("CHILD(%d) running command\n", getpid());
 
+		printf("background pid is %d\n", getpid());
+
 		// Source URL: Redirecting both Stdin and Stdout https://canvas.oregonstate.edu/courses/1987883/pages/exploration-processes-and-i-slash-o?module_item_id=24956228
 
 		if (curr_command->input_file){
@@ -215,11 +217,36 @@ void other_commands(struct command_line *curr_command)
 
 		}
 
-		// If the user doesn't redirect the standard input for a background command, then standard input must be redirected to /dev/null.
-		// If the user doesn't redirect the standard output for a background command, then standard output must be redirected to /dev/null.
+		
 		if (curr_command->is_bg){
 			
-			
+			// If the user doesn't redirect the standard input for a background command, then standard input must be redirected to /dev/null.
+			int sourceFD = open("/dev/null", O_RDONLY);
+			if (sourceFD == -1) { 
+				perror("source open()"); 
+				exit(1); 
+			}
+
+			int result = dup2(sourceFD, 0);
+			if (result == -1) { 
+				perror("source dup2()"); 
+				exit(2); 
+			}
+
+
+			// If the user doesn't redirect the standard output for a background command, then standard output must be redirected to /dev/null.
+			int targetFD = open("/dev/null", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			if (targetFD == -1) { 
+				perror("target open()"); 
+				exit(1); 
+			}
+
+			result = dup2(targetFD, 1);
+			if (result == -1) { 
+				perror("target dup2()"); 
+				exit(2); 
+			}
+
 
 		}
 
@@ -240,7 +267,6 @@ void other_commands(struct command_line *curr_command)
 
 		}
 
-		printf("background pid is %d/n", spawnPid);
 		break;
 	} 
 	
