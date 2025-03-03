a85cb0f7f627c7d715766ca8a5f977c095ad8285
Taylor L Beckham <97658753+tlbeckham@users.noreply.github.com>
2025-03-03 07:02:06 +0000

    cleaned up code and comments, submitting to gradescope

--- root/home/master/smallsh.c
+++ root/home/master/smallsh.c
@@ -3,9 +3,10 @@
 // Programming Assignment 4: SMALLSH
 
 /*
-*
-*
-*
+* This program is a shell that provides a prompt for running commands, executes built in
+* commands such as exit, cd, and status. It is also able to execute other commands by creating
+* new processes using execvp(), supports input and output redirection, and supports running
+* commands in the foreground and background processes.
 */
 
 
@@ -22,7 +23,10 @@
 #define INPUT_LENGTH 2048
 #define MAX_ARGS 512
 
-
+/*
+* Citation:
+* Adapted from sample parser on assignment page
+*/
 struct command_line
 {
 	char *argv[MAX_ARGS + 1];
@@ -32,7 +36,6 @@ struct command_line
 	bool is_bg;
 	
 };
-
 
 struct command_line *parse_input()
 {
@@ -44,6 +47,7 @@ struct command_line *parse_input()
 	fflush(stdout);
 	fgets(input, INPUT_LENGTH, stdin);
 
+	// record comment inputs as no args counted 
 	if (input[0] == '#'){
 		curr_command->argc = 0;
 		return curr_command;
@@ -69,11 +73,10 @@ struct command_line *parse_input()
 	
 }
 
-// shell must kill any other processes or jobs that your shell 
-// has started before it terminates itself.
+
+// shell must kill any other processes it has started then terminates itself
 void exit_command(struct command_line *curr_command)
 {
-	// printf("executing exit_command\n");
 	exit(0);
 
 }
@@ -81,29 +84,25 @@ void exit_command(struct command_line *curr_command)
 
 void cd_command(struct command_line *curr_command)
 {
-	// printf("executing cd_command\n");
-	
-	// char * getcwd (char *cwdbuf, size_t size );
-	// int chdir (const char *pathname);
-
-
-	// printf("curr_command arg count: %d\n", curr_command->argc);
-
-	// with no arguments - it changes to the directory specified in the HOME environment variable
+	// command with no arguments changes to the directory specified in the HOME environment variable
 	if(curr_command->argc == 1){
 		
-		// https://pubs.opengroup.org/onlinepubs/009696899/functions/getenv.html
+		/*
+		* Citation:
+		* Adapted from Getting the Value of an Environment Variable
+		* Source URL: https://pubs.opengroup.org/onlinepubs/009696899/functions/getenv.html
+		*/
 		const char *home_ev = "HOME";
 		char *home_directory;
 
 		home_directory = getenv(home_ev);
 
+		// change to home directory
 		chdir(home_directory);
 		 
 	}
 
-	// This command can also take one argument: the path of a directory to change to. Your cd command 
-	// must support both absolute and relative paths.
+	// if command has an argument change to that directory
 	else if(curr_command->argc == 2){
 
 		chdir(curr_command->argv[1]);
@@ -112,11 +111,11 @@ void cd_command(struct command_line *curr_command)
 
 }
 
+// track last exit code or signal status
 int last_status = 0;
 
 void status_command(struct command_line *curr_command)
 {
-	// printf("executing status_command\n");
 
 	printf("exit value %d\n", last_status);
 
@@ -128,11 +127,11 @@ struct bg_command {
 	pid_t pid;
 	char *command;
 	int status;
-	bool already_printed;
+	bool already_printed; // true or false if the background command has finished and been printed
 	struct bg_command* next; // pointer to next element in the linked list
 	};
 
-
+// create a background command structure
 struct bg_command* create_bg_command(pid_t pid, char *command, int status, bool already_printed) {
 
 	struct bg_command* curr_bg_command = malloc(sizeof(struct bg_command));
@@ -140,9 +139,9 @@ struct bg_command* create_bg_command(pid_t pid, char *command, int status, bool 
 	// Copy the value of pid into the pid in the structure
 	curr_bg_command->pid = pid;
 
-	// Allocate memory for curr_command in the structure
+	// Allocate memory for command in the structure
 	curr_bg_command->command = calloc(strlen(command) + 1, sizeof(char));
-	// Copy the value of curr_command into the curr_command in the structure
+	// Copy the value of command into the command in the structure
 	strcpy(curr_bg_command->command, command);
 
 	// Copy the value of pid into the pid in the structure
@@ -163,15 +162,8 @@ struct bg_command* head = NULL;
 struct bg_command* tail = NULL;
 
 
-/*
- * 
- * Citation:
- * Adapted from Using exec() with fork() in Module 6
- * Source URL: https://canvas.oregonstate.edu/courses/1987883/pages/exploration-process-api-executing-a-new-program?module_item_id=24956220
-*/
 void other_commands(struct command_line *curr_command)
 {
-	// printf("executing other_commands\n");
 
 	int childStatus;
 
@@ -179,27 +171,37 @@ void other_commands(struct command_line *curr_command)
 	pid_t childPid = fork();
 
 	struct bg_command* new_bg_command;
-  
+
 	// both parent and child execute next instruction after fork
 	switch(childPid){
+
+	/*
+	* Citation:
+	* Adapted from Using exec() with fork() in Module 6
+	* Source URL: https://canvas.oregonstate.edu/courses/1987883/pages/exploration-process-api-executing-a-new-program?module_item_id=24956220
+	*/
 	
-		case -1:
-		perror("fork()\n");
+	// fork failed
+	case -1:
 		last_status = 1;
 		exit(1);
 		break;
 
+	// child process executes this branch
 	case 0:
-
-		// Source URL: Redirecting both Stdin and Stdout https://canvas.oregonstate.edu/courses/1987883/pages/exploration-processes-and-i-slash-o?module_item_id=24956228
-
+		// if theres an input file for redirection 
 		if (curr_command->input_file){
+
+
+			/*
+			* Citation:
+			* Adapted from Redirecting both Stdin and Stdout
+			* Source URL: https://canvas.oregonstate.edu/courses/1987883/pages/exploration-processes-and-i-slash-o?module_item_id=24956228
+			*/
 
 			// Open the source file
 			int sourceFD = open(curr_command->input_file, O_RDONLY);
 			if (sourceFD == -1) { 
-				// perror("source open()"); 
-
 				printf("cannot open %s for input\n", curr_command->input_file);
 				last_status = 1;
 				exit(1); 
@@ -208,18 +210,19 @@ void other_commands(struct command_line *curr_command)
 			// Redirect stdin to source file
 			int result = dup2(sourceFD, 0);
 			if (result == -1) { 
-				perror("source dup2()"); 
+				//perror("source dup2()"); 
 				last_status = 2;
 				exit(2); 
 			}
 		}
 
+		// if theres an output file for redirection 
 		if (curr_command->output_file){
 
 			// Open target file
 			int targetFD = open(curr_command->output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 			if (targetFD == -1) { 
-				perror("target open()"); 
+				//perror("target open()"); 
 				last_status = 1;
 				exit(1); 
 			}
@@ -227,39 +230,37 @@ void other_commands(struct command_line *curr_command)
 			// Redirect stdout to target file
 			int result = dup2(targetFD, 1);
 			if (result == -1) { 
-				perror("target dup2()"); 
+				//perror("target dup2()"); 
 				last_status = 2;
 				exit(2); 
 			}
 
 		}
 
-		
+		// background commands are redirected to /dev/null if user doesn't redirect the standard input or output
 		if (curr_command->is_bg){
 			
-			// If the user doesn't redirect the standard input for a background command, then standard input must be redirected to /dev/null.
 			int sourceFD = open("/dev/null", O_RDONLY);
 			if (sourceFD == -1) { 
-				perror("source open()"); 
+				//perror("source open()"); 
 				exit(1); 
 			}
 
 			int result = dup2(sourceFD, 0);
 			if (result == -1) { 
-				perror("source dup2()"); 
+				//perror("source dup2()"); 
 				exit(2); 
 			}
 
-			// If the user doesn't redirect the standard output for a background command, then standard output must be redirected to /dev/null.
 			int targetFD = open("/dev/null", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 			if (targetFD == -1) { 
-				perror("target open()"); 
+				//perror("target open()"); 
 				exit(1); 
 			}
 
 			result = dup2(targetFD, 1);
 			if (result == -1) { 
-				perror("target dup2()"); 
+				//perror("target dup2()"); 
 				exit(2); 
 			}
 		}
@@ -270,17 +271,21 @@ void other_commands(struct command_line *curr_command)
 		printf("%s: no such file or directory\n", curr_command->argv[0]);
 		exit(2);
 		break;
-		
+
+	// parent process executes this branch
+	// Wait for child's termination
 	default:
-		// The parent process executes this branch
-		
-		// Wait for child's termination
 		if (!curr_command->is_bg){
 
 			childPid = waitpid(childPid, &childStatus, 0);
 
+			/*
+			* Citation:
+			* Adapted from Interpreting the Termination Status
+			* Source URL: https://canvas.oregonstate.edu/courses/1987883/pages/exploration-process-api-monitoring-child-processes?module_item_id=24956219
+			*/
 
-			// https://canvas.oregonstate.edu/courses/1987883/pages/exploration-process-api-monitoring-child-processes?module_item_id=24956219
+			// determine how child exited
 			if(WIFEXITED(childStatus)){
 				last_status = WEXITSTATUS(childStatus);
 			} 
@@ -295,19 +300,18 @@ void other_commands(struct command_line *curr_command)
 
 			printf("background pid is %d\n", childPid);
 
+			// if background command then add to linked list to track
 			new_bg_command = create_bg_command(childPid, curr_command->argv[0], childStatus, false);
 
 			// Add current background command to the linked list
 			if(head == NULL){
-				// This is the first element in the linked link
-				// Set the head and the tail to this element
+				// set the head and the tail to first element
 				head = new_bg_command;
 				tail = new_bg_command;
 			} 
 
 			else{
-				// This is not the first element. 
-				// Add this element to the list and advance the tail
+				// add node to the list and advance the tail
 				tail->next = new_bg_command;
 				tail = new_bg_command;
 			}
@@ -315,7 +319,6 @@ void other_commands(struct command_line *curr_command)
 		
 		break;
 	} 
-	
 }
 
 void bg_command_status(struct bg_command* list)
@@ -329,6 +332,7 @@ void bg_command_status(struct bg_command* list)
 		// check if bg command finished
 		pid_t finished_bg_command = waitpid(iList->pid, &iList->status, WNOHANG);
 
+		// if bg command finsihed and status has not yet been printed to the terminal
 		if(finished_bg_command > 0 && !iList->already_printed){
 
 			if(WIFEXITED(iList->status)){
@@ -342,12 +346,10 @@ void bg_command_status(struct bg_command* list)
 				printf("background pid %d is done: terminated by signal %d\n", iList->pid, last_status);
 				iList->already_printed = true;
 			}	
-			
 		}
 
 		iList = iList-> next;
     }
-
 }
 
 
@@ -358,10 +360,12 @@ int main()
 	while(true)
 	{
 
+		// check if any background processes have finished before returning command line
 		bg_command_status(head);
 
 		curr_command = parse_input();
 
+		// re-prompt when blank line or comment is input to command line
 		if(curr_command->argc == 0){
 			continue;
 		}
